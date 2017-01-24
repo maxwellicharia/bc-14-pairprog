98737c7cd50e13feb525c60d38aa4ed89e68b7d7
Maxwell-icharia <maxwellicharia@gmail.com>
2017-01-24 13:30:03 +0300

    Edit templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 
 from app import views
 
 app.config.from_object('config')
+app.config['SECRET_KEY'] = '2356';
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://example.db'
--- app/views.py
+++ app/views.py
@@ -1,11 +1,21 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, abort, session
 
 from app import app
 
+login_manager = LoginManager()
+
 @app.route('/')
 def index():
-    return render_template("login.html")
+    return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/signup', methods=['POST'])
+def signup():
+	session['signup'] = request.form['signup']
+    	return redirect(url_for('signup'))
+	return render_template("signup.html")
+
+# If user has signed up login immediately
+
+@app.route('/login', methods=['POST'])
+def login():
+	
