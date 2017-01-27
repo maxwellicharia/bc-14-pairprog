3f20d30868e173d8e67c81fd039c88f9e03dab26
Maxwell-icharia <maxwellicharia@gmail.com>
2017-01-27 07:29:15 +0300

    Update requirements.txt and README

--- README.md
+++ README.md
@@ -6,4 +6,15 @@
 * Users are able to develop remotely without ease and collaborate efficiently.
 * Users are able to initialise sessions in order to program.
 * Users are able to chat with other users in order to communicate effectively and invite others to their session to collaborate.
+* To install the much needed modules in order to run the pair programming session
 
+## Installation
+
+1. Clone the repository.
+2. In the cloned repository, open the terminal and pip install -r requirements.txt.
+
+## Running the app
+
+* Open in terminal
+* ls in order to see the files present and there is a run.py
+* In order to run it, 			python run.py
--- run.py
+++ run.py
@@ -28,17 +28,17 @@ def Login():
 
 	form = LoginForm()
 
-	error = None
-	if request.method == 'POST':
-	    if request.form['username'] != app.config['USERNAME']:
-	        error = 'Invalid username'
-	    elif request.form['password'] != app.config['PASSWORD']:
-	        error = 'Invalid password'
-	    else:
-	        session['logged_in'] = True
-	        flash('You were logged in')
-	        return redirect(url_for('show_entries'))
-	return render_template('login.html', error=error)
+	# error = None
+	# if request.method == 'POST':
+	#     if request.form['username'] != app.config['USERNAME']:
+	#         error = 'Invalid username'
+	#     elif request.form['password'] != app.config['PASSWORD']:
+	#         error = 'Invalid password'
+	#     else:
+	#         session['logged_in'] = True
+	#         flash('You were logged in')
+	#         return redirect(url_for('show_entries'))
+	return render_template('login.html')
 
 
 
